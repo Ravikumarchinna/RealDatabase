775be25ce2f38960a0f4a1e1f0bc803666f0d494
Ravikumar <test@youremail.com>
2021-07-11 20:32:12 +0530

    first commit

---
+++ RealmDatabase_Sample/Real_Model/Category.swift
@@ -0,0 +1,8 @@
+//
+//  Category.swift
+//  RealmDatabase_Sample
+//
+//  Created by Ravikumar on 7/10/21.
+//
+
+import Foundation
---
+++ RealmDatabase_Sample/Real_Model/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  RealmDatabase_Sample
+//
+//  Created by Ravikumar on 7/10/21.
+//
+
+import Foundation
